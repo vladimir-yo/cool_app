224686297e3680134b9d56f2269256a757e45af8
vladimir-yo <123456_8787@mail.ru>
2011-11-11 11:29:46 +0300

    c_1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolApp::Application.config.secret_token = 'c71978575889fd565e53f63856d77140541413f4e8c801e5c5f627932fb1299293bbcb9d2047cbc0c97b042381bed411512c22d0832f4407341946900b7037a9'
